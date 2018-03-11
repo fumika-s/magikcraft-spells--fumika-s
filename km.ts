f028809844966868a53780a8d9d2960bf426860d
fumika-s <veranoflor23@gmail.com>
2018-03-11 06:56:20 +0000

    Update km.ts

--- km.ts
+++ km.ts
@@ -3,8 +3,8 @@ const magik = magikcraft.io;
 function km() {
  magik.doNTimes(function(){
      magik.shakti();
-    },10,300);
+    },10,1000);
      magik.doNTimes(function(){
         magik.infierno();
-    },10,300);
+    },10,1000);
 }
